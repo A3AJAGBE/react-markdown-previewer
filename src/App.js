5a0b1b92dbad8e70ecf5303355291e30c2c9cc3e
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-11-23 21:21:17 +0000

    Add app structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card'
 
 const App = () => {
   return (
-    <h1>Markdown previewer</h1>
+    <Container fluid id="App-Container">
+        <header className="d-flex justify-content-center py-3">
+            <h1 className="text-center">A3AJAGBE Markdown Previewer </h1>
+        </header>
+
+        <Container fluid className="pb-4">
+            <Row xs={1} md={2} className="g-4" id="MP-Row">
+                <Col>
+                    <Card id="Editor-Card">
+                        <Card.Header> <h2>Editor</h2> </Card.Header>
+                        <Card.Body>
+                            <textarea id="editor"></textarea>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card id="Preview-Card">
+                        <Card.Header> <h2>Preview</h2> </Card.Header>
+                        <Card.Body id="preview">
+                        
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+
+    </Container>
   );
 }
 
